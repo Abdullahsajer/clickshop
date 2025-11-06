cc7d085fa9e3a43329f6d0c750f1e07342d7e44f
Abdullahsajer <infoaskit@gmail.com>
2025-11-06 11:23:37 +0300

    قبل التعديل على استايل الموقع

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    phone = forms.CharField(required=False, label="رقم الجوال")
-    address = forms.CharField(required=False, label="العنوان", widget=forms.Textarea)
+    phone = forms.CharField(required=False, label="رقم الجوال (اختياري)")
+    address = forms.CharField(required=False, widget=forms.Textarea, label="العنوان (اختياري)")
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = CustomUser
-        fields = ("username", "email", "phone", "address")
+        fields = ("username", "email", "phone", "address", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "input-field"})
